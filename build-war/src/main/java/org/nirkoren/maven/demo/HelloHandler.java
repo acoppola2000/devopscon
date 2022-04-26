65f4d04f2808dcdfcbe67138e65f2f2eaba23281
Annamaria Coppola <acoppola2000@libero.it>
2022-04-26 17:58:20 +0200

    personalizzazione

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Welcome, is it working from your Laptop?";
+		String hello = "Welcome Annamaria, is it working from your Laptop?";
 		return hello;
 	}
 
